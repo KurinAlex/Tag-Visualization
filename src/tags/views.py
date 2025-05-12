3c21598cb1d8c2b33186e848e70fc5805a4efece
Oleksii Kurin <olexkurin@gmail.com>
2025-05-12 20:28:23 +0300

    Create app layout for box drawing
    Added file upload form and corresponding view layout for just drawing boxes without tags

--- src/tags/views.py
+++ src/tags/views.py
@@ -1,3 +1,34 @@
+import json
+
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import JSONUploadForm
+
+
+def index(request):
+    rectangles = []
+    if request.method == "POST":
+        form = JSONUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data["json_file"]
+            data = json.load(file)
+
+            for obj in data:
+                min_x = obj["coordinates"]["min"]["x"]
+                min_y = obj["coordinates"]["min"]["y"]
+                max_x = obj["coordinates"]["max"]["x"]
+                max_y = obj["coordinates"]["max"]["y"]
+
+                rectangles.append(
+                    {
+                        "x": min_x,
+                        "y": min_y,
+                        "width": max_x - min_x,
+                        "height": max_y - min_y,
+                        "label": obj["coordinates"]["family_name"],
+                    }
+                )
+    else:
+        form = JSONUploadForm()
+
+    return render(request, "index.html", {"form": form, "rectangles": rectangles})
