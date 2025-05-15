17f856d7774deac4511b7e52eb2ed00259aace85
Oleksii Kurin <olexkurin@gmail.com>
2025-05-16 00:07:24 +0300

    Code clear up and formatting
    Remove unnecessary default-generated django files and settings. Formatted HTML template.

--- src/tags/apps.py
+++ src/tags/apps.py
@@ -1,6 +1,9 @@
+"""
+Tags app config
+"""
+
 from django.apps import AppConfig
 
 
 class TagsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tags'
+    name = "tags"
